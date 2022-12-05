def8ec9f205e2d034df54de6b47e68f7cc005e03
AlyZakaria <77212759+AlyZakaria@users.noreply.github.com>
2022-12-05 02:12:22 +0200

    server init

---
+++ server/src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+
+const app = express()
+const port = 3500
+
+app.use(bodyParser.json())
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`)
+})
+
+app.get('/', (_req, res) => res.send('Home Page..'))
