25c3c50ef2674dd5b28fff6db425ad107de4e84c
AlyZakaria <77212759+AlyZakaria@users.noreply.github.com>
2022-12-08 13:59:44 +0200

    test

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,32 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-
+import cors from 'cors'
 const app = express()
 const port = 3500
 
 app.use(bodyParser.json())
+
+app.use(cors())
+// We set the CORS origin to * so that we don't need to
+// worry about the complexities of CORS.
+app.use(
+    cors({
+        allowedHeaders: [
+            'Origin',
+            'X-Requested-With',
+            'Content-Type',
+            'Accept',
+            'X-Access-Token',
+            'Authorization',
+            'Access-Control-Allow-Origin',
+            'Access-Control-Allow-Headers',
+            'Access-Control-Allow-Methods',
+        ],
+        methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
+        preflightContinue: true,
+        origin: '*',
+    })
+)
 
 app.listen(port, () => {
     console.log(`listening on ${port}`)
@@ -12,4 +34,10 @@ app.listen(port, () => {
 
 app.get('/', (_req, res) => res.json('Home Page..'))
 
+app.post('/login', (req: express.Request, res: express.Response) =>
+    res.json(req.body)
+)
+app.post('/register', (req: express.Request, res: express.Response) =>
+    res.json(req.body)
+)
 export default app
