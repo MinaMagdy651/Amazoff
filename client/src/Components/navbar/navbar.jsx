9953c06690d653dee57374d9334a1f4733d4168a
Mina Magdy <72947628+amwopps@users.noreply.github.com>
2022-12-29 19:30:28 +0200

    preliminary navbar added

--- client/src/Components/navbar/navbar.jsx
+++ client/src/Components/navbar/navbar.jsx
@@ -1,23 +1,79 @@
 import { Link } from "react-router-dom";
-function Navbar() {
+import "bootstrap/dist/css/bootstrap.css";
+
+function Navbar_() {
   return (
-    <div>
-      <ul>
-        <Link to="/home">
-          <li>Home</li>
-        </Link>
-        <Link to="/counter">
-          <li>Counter</li>
-        </Link>
-        <Link to="/login">
-          <li>Login</li>
-        </Link>
-        <Link to="/register">
-          <li>Register</li>
-        </Link>
-      </ul>
+    <div id="header" className="sticky-top">
+      <nav
+        className="navbar navbar-expand-lg bg-body-tertiary"
+        data-bs-theme="dark"
+      >
+        <div className="container-fluid">
+          <Link to="/home" className="navbar-brand">
+            Amazoff
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link
+                  to="/home"
+                  className="nav-link active"
+                  aria-current="page"
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/counter"
+                  className="nav-link active"
+                  aria-current="page"
+                >
+                  Counter
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link
+                  to="/login"
+                  className="nav-link active"
+                  aria-current="page"
+                >
+                  Login
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link
+                  to="/register"
+                  className="nav-link active"
+                  aria-current="page"
+                >
+                  Register
+                </Link>
+              </li>
+            </ul>
+            <form className="d-flex" role="search">
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
 
-export default Navbar;
+export default Navbar_;
