e4c5972d96ed8671d520dd9369d53b26f99d2561
Mina Magdy <72947628+amwopps@users.noreply.github.com>
2023-01-01 22:06:08 +0200

    Reviews

--- client/src/Components/product/product.jsx
+++ client/src/Components/product/product.jsx
@@ -1,11 +1,59 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 import useGetProduct from "../../shared/useGetProduct";
+import Review from "../review/review";
 
 function Product(probs) {
+  const reviews = [
+    {
+      customer_name: "Mina",
+      review_id: 1,
+      title: "Title",
+      description: "gamed awy",
+      rating: 4.5,
+    },
+
+    {
+      customer_name: "msh mina",
+      review_id: 2,
+      title: "another title",
+      description: "",
+      rating: 2,
+    },
+
+    {
+      customer_name: "aloyka",
+      review_id: 3,
+      title: "by2olo 7lw",
+      description:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempore, optio. Quibusdam, quia explicabo assumenda distinctio iusto eveniet quisquam non perferendis eaque? Fuga esse, atque dolorum reprehenderit rem minus perferendis numquam.",
+      rating: 3.5,
+    },
+  ];
+  const [product, setProduct] = useState({});
   const id = useParams().id;
-  console.log(id);
-  useGetProduct(id);
-  return <h1>{id}</h1>;
+  const p = useGetProduct(id);
+  useEffect(() => {
+    async function execute() {
+      try {
+        const value = await p;
+        if (value) setProduct(value);
+        else setProduct({});
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    execute();
+    // eslint-disable-next-line
+  }, []);
+  console.log(product);
+  return (
+    <div className="container">
+      <h1>{id}</h1>
+      {product.product_id}
+      <Review reviews={reviews}></Review>
+    </div>
+  );
 }
 
 export default Product;
--- client/src/shared/useSearchProduct.js
+++ client/src/shared/useSearchProduct.js
@@ -1,8 +1,11 @@
 import axios from "../APIS/axios";
 const PRODUCT_SEARCH = "/product?name=";
 
+function isEmptyOrSpaces(str) {
+  return str === null || str.match(/^ *$/) !== null;
+}
 const useSearchProduct = (data) => {
-  if (data !== "") {
+  if (!isEmptyOrSpaces(data)) {
     return axios
       .get(PRODUCT_SEARCH + data.toLowerCase())
       .then((response) => response.data)
