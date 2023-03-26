ec8404dc43e35f1e0101e1d131b23c9997871d52
Mina Magdy <72947628+amwopps@users.noreply.github.com>
2023-03-26 06:11:34 +0200

    useAddReview

--- client/src/shared/useAddReview.js
+++ client/src/shared/useAddReview.js
@@ -1,19 +1,30 @@
 import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
 import { updateReviewsAction } from "../Redux/shopSlicer";
 import axios from "../APIS/axios";
-const ADD_REVIEW = "/product/addreview";
+import urls from "../APIS/url.json";
+const URL = urls.addReview;
 const useAddReview = (data) => {
   const dispatch = useDispatch();
-  if (Object.keys(data).length !== 0) {
-    axios.defaults.headers.post["Authorization"] =
-      "Bearer " + sessionStorage.getItem("access_token");
-    axios
-      .post(ADD_REVIEW, data)
-      .then(() => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const postData = async () => {
+    try {
+      const response = await axios.post(URL, data);
+      if (response.status === 200) {
         dispatch(updateReviewsAction(data.product_id));
-      })
-      .catch((error) => console.log(error));
-  }
+        setLoading(false);
+        setError(false);
+      }
+    } catch {
+      setError(true);
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    if (Object.keys(data).length !== 0) postData();
+  });
+  return { loading, error };
 };
 export default useAddReview;
 //title description rating product_id customer_id
